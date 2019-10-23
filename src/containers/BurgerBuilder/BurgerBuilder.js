c664e2ce55365e7f0482961d4a97f652e283ceca
Marcel Balint <blank_blankness@yahoo.com>
2019-10-23 03:31:11 -0400

    Displaying and updating the burger price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -68,6 +68,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHadler}
           disabled={disabledInfo}
+          price={this.state.totalPrice}
         />
       </Auxiliary>
     );
