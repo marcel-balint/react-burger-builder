06b320fdfa5041598ec1f0d88f924654c9eab4ec
Marcel Balint <blank_blankness@yahoo.com>
2019-10-23 07:04:26 -0400

    Added a logo in the application

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import LogoImage from "../../assets/images/burger-logo.png";
+import classes from "./Logo.css";
+
+const Logo = props => {
+  return (
+    <div className={classes.Logo}>
+      <img src={LogoImage} alt="MyBurger"></img>
+    </div>
+  );
+};
+
+export default Logo;
