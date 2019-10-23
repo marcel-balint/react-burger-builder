7cb159db196d429b699cd20dbb481fd5885dea59
Marcel Balint <blank_blankness@yahoo.com>
2019-10-23 07:33:14 -0400

    Adding reusable navigation items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const NavigationItems = props => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link="/" active>
+        Burger Buider
+      </NavigationItem>
+      <NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
+  );
+};
+
+export default NavigationItems;
