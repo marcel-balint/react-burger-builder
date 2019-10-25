40308c0d19bfedc06dc460e4785755624c97df1d
Marcel Balint <blank_blankness@yahoo.com>
2019-10-25 07:01:49 -0400

    Outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,11 +3,38 @@ import React from "react";
 import classes from "./Order.css";
 
 const Order = props => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #000",
+          padding: "5px",
+          borderRadius: "4px"
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad(1)</p>
+      <p>Ingredients: {ingredientOutput}</p>
       <p>
-        Price: <strong>USD 5.55</strong>
+        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
       </p>
     </div>
   );
