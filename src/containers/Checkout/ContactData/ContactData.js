8b0bc0a1bb073292964323ff79a4df3299841b48
Marcel Balint <blank_blankness@yahoo.com>
2019-10-25 08:21:21 -0400

    Handling user input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,6 +82,18 @@ class ContactData extends Component {
       });
   };
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedForm[inputIdentifier] = updatedFormElement;
+    this.setState({ orderForm: updatedForm });
+  };
+
   render() {
     const formElementsArry = [];
     for (let key in this.state.orderForm) {
@@ -98,6 +110,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>
